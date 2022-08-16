04511dea689910befcb414e15a5825602557d32b
Lekan <olalekancrown11@gmail.com>
2022-08-16 16:24:10 -0400

    First commit

---
+++ public/js/main.js
@@ -0,0 +1,107 @@
+const deleteBtn = document.querySelectorAll('.fa-trash')
+const item = document.querySelectorAll('.item span')
+const itemCompleted = document.querySelectorAll('.item span.completed')
+const addContactBtn = document.querySelector('.add-contact')
+const cancelAddBtn = document.querySelector('#discard')
+const form = document.querySelector('form')
+const editContactBtn = document.querySelectorAll('.edit')
+const nam = document.querySelector('#name')
+
+nam.addEventListener('click', logit)
+
+function logit(e) {
+    console.log(e);
+}
+addContactBtn.addEventListener('click', addContactForm)
+cancelAddBtn.addEventListener('click', discardContact)
+
+Array.from(deleteBtn).forEach((element)=>{
+    element.addEventListener('click', deleteItem)
+})
+
+Array.from(item).forEach((element)=>{
+    element.addEventListener('click', markComplete)
+})
+
+Array.from(editContactBtn).forEach((element)=>{
+    element.addEventListener('click', editContact)
+})
+
+function addContactForm() {
+    form.reset()
+    form.style.display = 'inline-block'
+    form.style.zIndex = '999'
+    form.style.position = 'absolute'
+    form.style.top = '50%'
+    form.style.left = '50%'
+    form.style.transform = 'translate(-50%, -50%)';
+    form.style.backgroundColor = 'white'
+}
+
+function discardContact() {
+    // e.preventDefault()
+    form.reset()
+    form.style.display = 'none'
+    // this.parentNode.parentNode.style.display = 'none'
+}
+
+async function deleteItem(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('deleteItem', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markComplete(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('markComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+ async function editContact(e){
+    // const contact = this.parentNode.parentNode.parentNode.childNodes[1].innerTex
+    const contact = e.target.attributes[0].value
+
+    console.log(e.target.attributes[0].value);
+    // form.style.display = 'block'
+    try{
+        const response = await fetch('editContact',  {
+            method: 'get',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': contact
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
---
+++ server.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 2121
+require('dotenv').config()
+
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'contacts';
+    
+   
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+        // db.createCollection("counters");
+    })
+    
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+app.get('/',async (request, response)=>{
+    const contact = await db.collection('contacts').find().toArray()
+    // const itemsLeft = await db.collection('contacts').countDocuments({completed: false})
+    // response.render('index.ejs', { contacts: todoItems, left: itemsLeft })
+    response.render('index.ejs', { person: contact })
+    // db.collection('todos').find().toArray()
+    // .then(data => {
+    //     db.collection('todos').countDocuments({completed: false})
+    //     .then(itemsLeft => {
+    //         response.render('index.ejs', { items: data, left: itemsLeft })
+    //     })
+    // })
+    // .catch(error => console.error(error))
+})
+
+app.get('/editContact', async (req, res) => {
+    const contact = await db.collection('contacts').find({ _id: req.body.itemFromJS })
+    // const contact = await db.collection('contacts').find({ name: req.body.telNum })
+    // res.render('index.ejs', { editForm: contact })
+    console.log(contact);
+})
+
+app.post('/addContact', (request, response) => {
+    const body = request.body
+    db.collection('contacts').insertOne({name: body.name, telNum: body.telNum, email: body.email})
+    .then(result => {
+        console.log(request.body)
+        response.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+
+app.put('/jh', (request, response) => {
+    // db.collection('contacts').updateOne({thing: request.body.itemFromJS},{
+    //     $set: {
+    //         completed: true
+    //       }
+    // },{
+    //     sort: {_id: -1},
+    //     upsert: false
+    // })
+    // .then(result => {
+    //     console.log('Marked Complete')
+    //     response.json('Marked Complete')
+    // })
+    // .catch(error => console.error(error))
+    console.log(request.body.contactEdit);
+
+})
+
+app.put('/markUnComplete', (request, response) => {
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        $set: {
+            completed: false
+          }
+    },{
+        sort: {_id: -1},
+        upsert: false
+    })
+    .then(result => {
+        console.log('Marked Complete')
+        response.json('Marked Complete')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.delete('/deleteContact', (request, response) => {
+    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
+    .then(result => {
+        console.log('Todo Deleted')
+        response.json('Todo Deleted')
+    })
+    .catch(error => console.error(error))
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
